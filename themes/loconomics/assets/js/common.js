391d40a9b11b6b32f780128d8cc3e98202853a8a
IagoSRL <iagosrl@gmail.com>
2018-10-19 16:28:55 +0200

    Moved js from old trial into theme, added to bundle

--- themes/loconomics/assets/js/common.js
+++ themes/loconomics/assets/js/common.js
@@ -1,4 +1,5 @@
-(global => {
+/* white line to prevent error with Hugo resources.Concat */
+(function(global) {
     let list = ['images/hero1.jpg', 'images/hero2.jpg', 'images/hero3.jpg'];
     let i = 0;
 
@@ -6,7 +7,6 @@
     //     document.getElementById('div-home-hero-bg').style.backgroundImage = 'url("' + list[i] + '")';
     //     if (i >= list.length - 1) i = 0; else i++;
     // }, 5000);
-
 
     let lastScrollPos = 0;
     let ticking = false;
@@ -17,13 +17,16 @@
       if (!ticking) {
         window.requestAnimationFrame(function() {
           let element = document.getElementById("primary-nav");
-          (lastScrollPos >= 100) ?
-            element.className = element.className.replace(/\bbg-transparent\b/g, "") :
+          if (lastScrollPos >= 100) {
+            element.className = element.className.replace(/\bbg-transparent\b/g, "");
+          }
+          else {
             element.classList.add("bg-transparent");
+          }
 
-            if (lastScrollPos >= 100) {
-              //element.classList.toggle("fadeOut");
-            }
+          if (lastScrollPos >= 100) {
+            //element.classList.toggle("fadeOut");
+          }
           ticking = false;
         });
 
