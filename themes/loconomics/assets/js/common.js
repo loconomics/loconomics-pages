d467c100225ec0d9bb9190aa7280f188f716a83f
IagoSRL <iagosrl@gmail.com>
2018-11-08 23:13:37 +0100

    Completed and enabled rotation of hero image (for home) #4

--- themes/loconomics/assets/js/common.js
+++ themes/loconomics/assets/js/common.js
@@ -1,12 +1,18 @@
 /* white line to prevent error with Hugo resources.Concat */
 (function(global) {
-    let list = ['images/hero1.jpg', 'images/hero2.jpg', 'images/hero3.jpg'];
-    let i = 0;
 
-    // setInterval(function(){
-    //     document.getElementById('div-home-hero-bg').style.backgroundImage = 'url("' + list[i] + '")';
-    //     if (i >= list.length - 1) i = 0; else i++;
-    // }, 5000);
+  window.addEventListener('DOMContentLoaded', function () {
+      const list = ['/images/hero/hero2.jpg', '/images/hero/hero3.jpg', '/images/hero/hero1.jpg'];
+      const hero = document.querySelector('#home .HeroSection');
+      let i = 0;
+      if (hero) {
+        setInterval(function(){
+            hero.style.backgroundImage = 'url("' + list[i] + '")';
+            if (i >= list.length - 1) i = 0;
+            else i++;
+        }, 5000);
+      }
+    });
 
     let lastScrollPos = 0;
     let ticking = false;
